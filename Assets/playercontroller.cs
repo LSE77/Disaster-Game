4a4f253fbad552851c5b3db1b6cfb2f24c336598
LSE77 <seokhwan.lee.s@gmail.com>
2025-07-25 03:56:13 +0900

    1.0.4
    1인칭으로 수정
    wasd로 움직이며 마우스로 시야 조정

--- Assets/playercontroller.cs
+++ Assets/playercontroller.cs
@@ -5,13 +5,14 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
+
     private Rigidbody rb;
     private bool isGrounded;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true; // 추가: 넘어짐 방지
+        rb.freezeRotation = true; // 넘어짐 방지
     }
 
     void Update()
@@ -29,8 +30,9 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal"); // A/D
         float moveZ = Input.GetAxis("Vertical");   // W/S
 
-        Vector3 move = new Vector3(moveX, 0, moveZ) * moveSpeed;
-        Vector3 newVelocity = new Vector3(move.x, rb.velocity.y, move.z);
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        Vector3 newVelocity = move * moveSpeed;
+        newVelocity.y = rb.velocity.y; // y축은 점프 중력 유지
 
         rb.velocity = newVelocity;
     }
